b3fde99f0949dff441fdda70828ea9646149cea7
BlueSky <junstti>
2025-03-27 16:46:17 +0700

    new1

--- api/play_btn.php
+++ api/play_btn.php
@@ -1,14 +1,25 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: https://frontend-publish.pages.dev"); // Chỉ cho phép frontend của bạn
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Xử lý POST request ở đây
-} else {
-    http_response_code(405); // Trả về lỗi 405 nếu không phải POST
-    echo "Method Not Allowed";
+// Xử lý preflight request
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(204);
+    exit;
+// }
+
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Methods: POST");
+// header("Access-Control-Allow-Headers: Content-Type");
+// header("Content-Type: application/json");
+
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//     // Xử lý POST request ở đây
+// } else {
+//     http_response_code(405); // Trả về lỗi 405 nếu không phải POST
+//     echo "Method Not Allowed";
 }
 
 $image_url = $_POST['image_url'] ?? '';
